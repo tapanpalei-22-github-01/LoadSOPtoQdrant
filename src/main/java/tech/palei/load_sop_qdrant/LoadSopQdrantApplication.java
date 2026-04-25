e947c9dacf83627ae67d3732057188e45124dc01
tapankumar.palei <tapankumar.palei@e2open.com>
2026-04-25 14:58:22 +0000

    first commit

---
+++ src/main/java/tech/palei/load_sop_qdrant/LoadSopQdrantApplication.java
@@ -0,0 +1,13 @@
+package tech.palei.load_sop_qdrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadSopQdrantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadSopQdrantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/palei/load_sop_qdrant/LoadSopQdrantApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.palei.load_sop_qdrant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadSopQdrantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
